b09c9f58c7fac261d70a9cb44ee11f328c16aa41
Jihwan Lim <ghwn5936@gmail.com>
2020-04-19 20:35:11 +0900

    Add test for get_absolute_url() method

--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -7,6 +7,12 @@ from ..models import Cheese
 pytestmark = pytest.mark.django_db
 
 
+def test_get_absolute_url():
+    cheese = CheeseFactory()
+    url = cheese.get_absolute_url()
+    assert url == f'/cheeses/{cheese.slug}/'
+
+
 def test___str__():
     cheese = CheeseFactory()
     assert cheese.__str__() == cheese.name
